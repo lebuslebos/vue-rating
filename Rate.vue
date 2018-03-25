dfefe352bc2f8f61da161dfde45db892af921205
lebuslebos <34952749+lebuslebos@users.noreply.github.com>
2018-03-25 21:23:35 +0800

    Update and rename review-star.vue to Rate.vue

--- Rate.vue
+++ Rate.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
+        <input type="hidden" name="rate" v-model="rate">
         <small v-text="hintArr[enter]"></small>
         <div>
             <i class="fa fa-star"
                v-for="n in 7"
                :class="{'enter':n<=enter}"
                @mouseenter="onEnter(n)"
-               @mouseleave="onLeave(n)"
+               @mouseleave="onLeave()"
                @click="setRate(n)"
             ></i>
             &nbsp;<span :class="{'enter':rate!==0}">{{enter}}</span>
@@ -16,12 +17,12 @@
 
 <script>
     export default {
-        name: "review-star",
+        name: "rate",
         data() {
             return {
                 enter: 0,
                 rate: 0,
-                hintArr: ['打分必须要填的','史上第一差', '很差', '比较差', '马马虎虎', '值得一用', '推荐用', '万年回购'],
+                hintArr: ['打分必须要填的', '史上第一差', '很差', '比较差', '马马虎虎', '值得一用', '推荐用', '万年回购'],
             }
         },
         methods: {
@@ -29,7 +30,7 @@
                 this.enter = n;
                 this.hint = this.hintArr[n]
             },
-            onLeave(n) {
+            onLeave() {
                 this.enter = this.rate;
             },
             setRate(n) {
@@ -40,10 +41,11 @@
 </script>
 
 <style scoped>
-    .fa-star,span{
+    .fa-star, span {
         font-size: 2rem;
         color: #DADADA;
     }
+
     .fa-star {
         cursor: pointer;
     }
@@ -51,5 +53,4 @@
     .enter {
         color: #FFAA00;
     }
-
 </style>
